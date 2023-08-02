ffae7c965f9d6dcd98b1f5759b19aca54d3e7fb8
vishnu <vishnuv9677@gmail.com>
2023-08-02 23:22:36 +0530

    gfinal

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -28,7 +28,7 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        if(sum < 10){
+        if(digits*9 < sum){
             throw new Exception("Account Number can not be generated");
         }
         String accountNumber ="";
